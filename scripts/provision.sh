71c57b6218c04be282972b3408053f80530cb0ce
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-11-27 14:59:46 +0200

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -14,12 +14,12 @@ which wget unzip &>/dev/null || {
 
 which terraform &>/dev/null || {
   pushd /usr/local/bin
-  wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
-  unzip terraform_0.11.10_linux_amd64.zip
+  sudo wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+  sudo unzip terraform_0.11.10_linux_amd64.zip
   popd
 }
 
 # install git
 which git &>/dev/null || {
-  apt-get install -y git
+  sudo apt-get install -y git
 }
