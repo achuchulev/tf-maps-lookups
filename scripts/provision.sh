9d01910ac80c07afb814ceda49c8b9eb81e8ce92
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-11-27 14:46:45 +0200

    Create provision.sh

---
+++ scripts/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+which curl docker make ruby || {
+  sudo apt-get update
+  sudo apt-get install -y curl docker.io make ruby
+  sudo usermod -a -G docker ubuntu
+  sudo gem install rubyzip
+}
+
+# install terraform
+which wget unzip &>/dev/null || {
+  apt-get install -y wget unzip
+}
+
+which terraform &>/dev/null || {
+  pushd /usr/local/bin
+  wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+  unzip terraform_0.11.10_linux_amd64.zip
+  popd
+}
+
+# install git
+which git &>/dev/null || {
+  apt-get install -y git
+}
